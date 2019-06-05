e55217952d08317df7959255f3f428b7860b2b26
divyanshugarg36 <divyanshugarg36@github.com>
2019-06-05 20:53:18 +0530

    Made some minor updates
    I just added number in each button from  parent component to  child component by using this.props, even added a onclick event on button to verify its working.

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import * as serviceWorker from './serviceWorker';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="square" onClick={()=>alert('test')}>
+        {this.props.number}
       </button>
     );
   }
@@ -15,7 +15,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square number={i}/>;
   }
 
   render() {
